c02198b9d9a816e528a698b5e636802efb770477
LsGeeker <143838345+LsGeeker@users.noreply.github.com>
2024-04-19 12:45:41 -0400

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+<script>
+window.addEventListener("load", function() {
+    // Sélectionne le loader
+    var loader = document.querySelector(".loader-wrapper");
+    // Cache le loader
+    loader.style.display = "none";
+});
+</script>
